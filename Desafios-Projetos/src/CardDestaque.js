a92c7d5ad0d23ab649c851d76c960fcebdff21f4
AndreFeh <andrefelipe.c.santos@gmail.com>
2024-09-18 23:42:49 -0300

    alteracoes projeto JS

--- Desafios-Projetos/src/CardDestaque.js
+++ Desafios-Projetos/src/CardDestaque.js
@@ -11,7 +11,8 @@ class CardDestaque extends HTMLElement{
     dtq-img="url-da-imagem.jpg" 
     dtq-alt="Descrição da imagem" 
     nome="Nome do Anime" 
-    categoria="Categoria do Anime">
+    categoria="Categoria do Anime"
+    link-anime="link-do-anime.html">
 </card-destaque>
 */
 
@@ -19,6 +20,13 @@ class CardDestaque extends HTMLElement{
         const componentRoot = document.createElement("div");
         componentRoot.setAttribute("class", "card__destaque");
 
+        /* Criando a tag <a> */
+        const linkAnime = document.createElement("a");
+        // Obtendo o valor do atributo 'link-anime' ou, se não existir, usando 'index.html' como valor padrão
+        const hrefValue = this.getAttribute("link-anime") || "index.html";  // Link definido pelo usuário ou padrão para `index.html`
+        linkAnime.setAttribute("href", hrefValue); // Define o link corretamente
+        linkAnime.setAttribute("class", "card__link");
+            
         /*ESSES CARDS TERAO IMAGEM DIV1, DIV2 SENDO NOME, CATEGORIA*/
         const cardUp = document.createElement("div");
         cardUp.setAttribute("class", "card__up");
@@ -39,8 +47,15 @@ class CardDestaque extends HTMLElement{
         cardDown.appendChild(nome);
         cardDown.appendChild(categoria);
         
+        /* Adicionando partes do card dentro do link */
+        linkAnime.appendChild(cardUp);
+        linkAnime.appendChild(cardDown);
+        
+        /* SE  OS CARDS FICAREM DENTRO DE COMPONENT ROOT, O LINK NAO IRA FUNCIONAR 
         componentRoot.appendChild(cardUp);
-        componentRoot.appendChild(cardDown);
+        componentRoot.appendChild(cardDown);        
+        */
+        componentRoot.appendChild(linkAnime);
 
         return componentRoot
     }
@@ -50,20 +65,30 @@ class CardDestaque extends HTMLElement{
         style.textContent = `
             .card__destaque{
                 display: flex;
-                flex-direction: row;
-                border: 1px solid #ccc; 
+                flex-direction: column;
+                border: 1px solid #ccc;
+                 
                 padding: 10px; 
                 border-radius: 5px; 
-                background-color: #fff;
+                background-color: black;
+                color: white;
                 width: 100px; 
                 height: 100px;
+            }
 
+            .card__link {
+                display: block;
+                text-decoration: none; /* Remove o sublinhado do link */
+                color: inherit; /* Garante que o texto do link herde a cor do card */
+                width: 100%;
+                height: 100%;
             }
 
             .card__up{
                 display: flex;
                 flex-direction: column;
                 align-items: center;
+                
             }
 
             .card__up img{
@@ -75,9 +100,11 @@ class CardDestaque extends HTMLElement{
             .card__down{
                 display: flex;
                 flex-direction: column;
+
             }
 
             .card__down h3{}
+            
             .card__down span{}
         `;
         return style;
